176bf7e65446062289499ffdad19a91cc722a03f
audrbs <bbmk3137@gmail.com>
2024-11-06 22:33:32 +0900

    Updated ex3-7.sh with corrected symbolic link logic

--- src/ex3-7.sh
+++ src/ex3-7.sh
@@ -9,9 +9,9 @@ else
    i=0
    while [ $i -lt 5 ]
    do
-       mkdir file$i
+       mkdir "file$i"
        touch "file$i.txt"
-       ln -s file$i.txt file$i/file$i.txt
+       ln -s "file$i.txt" "file$i/file$i.txt"
        i=$((i + 1))
    done
 
