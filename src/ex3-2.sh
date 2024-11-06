d11bc5cf85abd1f814b4a1cf18d8f0c5f8d7c635
audrbs <bbmk3137@gmail.com>
2024-11-06 16:42:58 +0900

    Update ex3-2.sh to add arithmetic operations

--- src/ex3-2.sh
+++ src/ex3-2.sh
@@ -4,4 +4,15 @@ num=`expr $1 $2 $3`
 
 echo "$num"
 
-exit 0s
+exit 0
+#!/bin/sh
+num1=$1
+operator=$2
+num2=$3
+
+if [ "$operator" = "+" ]; then
+	result=`expr $num1 + $num2`
+elif [ "$operator" = "-" ]; then
+	result=`expr $num1 - $num2`
+fi
+echo $result
