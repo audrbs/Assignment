610ba250d31b1244a7dbda45150a5a4fc2fadcfe
audrbs <bbmk3137@gmail.com>
2024-11-08 12:16:49 +0900

    Update ex3-6.sh with new code

--- src/ex3-6.sh
+++ src/ex3-6.sh
@@ -2,10 +2,10 @@
 
 if [ -d "$1" ]
 then
-   :
+   exit 0
 else
-   mkdir $1
-   cd $1
+   mkdir "$1"
+   cd "$1"
    i=0
    while [ $i -lt 5 ]
    do
@@ -13,14 +13,18 @@ else
        i=$((i + 1))
    done
 
-   tar cvf $1.tar file0.txt file1.txt file2.txt file3.txt file4.txt
+   tar cf "$1.tar" file0.txt file1.txt file2.txt file3.txt file4.txt
 
-   mkdir $1
+   mkdir "$1"
    
-   mv $1.tar $1
-   cd $1
-   tar xvf "$1.tar"
+   mv "$1.tar" "$1"
+   cd "$1"
+   tar xf "$1.tar" > /dev/null  
 
-fi 
+   echo $(ls)
+
+   cd ../..
+   ls "$1"/*.txt | xargs -n 1 basename
+fi
 
 exit 0
